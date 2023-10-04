9ac96a9378aed4d9a7872e977fa4c00eda721c77
danniica09 <126630739+danniica09@users.noreply.github.com>
2023-10-03 19:58:49 -0500

    Taller segundo Semestre 1.0.1
    Se crea el Taller se llaman las librerias stdio.h y string.h con la cual se puede contar el numero de caracteres una vez con esto con los if se revisa si el producto tiene descuento

---
+++ Taller segundo Semestre.c
@@ -0,0 +1,55 @@
+// (i) el código de barra del producto; (ii) el código del
+//almacén. El código de barra del producto corresponde a una cadena
+//alfanumérica de 10 dígitos. El código del almacén corresponde a una
+//cadena alfanumérica de 5 dígitos, empezando por el prefijo 20.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char Product_Bar[11]; //Se usan condicionales11
+    char Store_Bar[6];
+    
+    // Codigo del producto
+    while (1) {
+        printf("Por favor, ingrese el codigo de barras del producto: \n");
+        scanf("%s", Product_Bar); // Leemos una cadena de caracteres
+        
+        if (strlen(Product_Bar) == 10) {	//Mediante el strlen se comprueba la longitud
+		printf("Correcto.\n");
+		
+			if (Product_Bar == 8949899430);		
+            printf("Su producto tiene 20 porciento de descuento\n");
+            
+            if (Product_Bar == 7653512593);
+            printf("Su producto tiene 20 porciento de descuento\n");
+			break;
+			
+			if (Product_Bar == 9383867373);
+            printf("Su producto tiene 20 porciento de descuento\n");
+			break;
+			
+			if (Product_Bar == 88392904209);
+            printf("Su producto tiene 20 porciento de descuento\n");
+			break;
+        }
+        
+        else
+            printf("El codigo de barras del producto debe tener exactamente 10 digitos.\n");
+            break;
+        }
+    }
+    
+    // Codigo de la tienda
+    printf("Por favor, ingrese el codigo de barras del almacen: \n");
+    scanf("%c", Store_Bar); 
+
+    if (strlen(Store_Bar) != 5 || strncmp(Store_Bar, "20", 2) != 0) { 							//strlen(Store_Bar) != 5: Esto calcula la longitud de la cadena de caracteres 
+        printf("El codigo de barras del almacen debe tener 5 digitos y comenzar con '20'.\n");	//strncmp(Store_Bar, "20", 2) != 0 verifica que los primeros 2 caracteres de Store_Bar no sean "20".
+    } else {
+        // Si llegamos aquí, ambas cadenas son válidas
+        printf("Codigos de barras validos: Producto: %s, Almacen: %s\n", Product_Bar, Store_Bar);
+    }
+
+    return 0;
+}
